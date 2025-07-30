d56395981c8370fdd472066cffe89bb4e0711158
kvasa02 <kvasa@ucsc.edu>
2025-07-30 13:33:53 -0700

    some updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+
 import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: '/<your-repository-name>/',
+  plugins: [react()],
+})
+
